21c35d2de3d5ce0a523a33c9c39d7b8b78105272
brivadeneira <brivadeneira@protonmail.com>
2016-09-26 10:57:25 -0300

    Commit inicial

--- Servidor_Hora.py
+++ Servidor_Hora.py
@@ -8,7 +8,7 @@
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
+#  the Free Software Foundation; either version 3 of the License, or
 #  (at your option) any later version.
 #  
 #  This program is distributed in the hope that it will be useful,
@@ -35,7 +35,7 @@ def main(): #definición de la fción principal.
     SERVIDOR HORA (v1.0.0)
     CURSO: Introducción al lenguaje de programación python.
     - Ejemplo socket TCP -
-    Licencia GNU GPL v2
+    Licencia GNU GPL v3
     ******************************************
     """
     try:
